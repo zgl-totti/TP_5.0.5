bdfc1a67de77ca6114fb72246c0d92ee8d8b2f2d
totti <724249769@qq.com>
2019-04-09 11:39:59 +0800

    201904091139

---
+++ application/common/lib/Aes.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\common\lib;
+
+class Aes
+{
+    private $aes_key;
+    private $aes_iv;
+
+    function __construct()
+    {
+        $this->aes_key = hash('sha256', config('app.aes_key'), true);
+        $this->aes_iv = substr(md5(config('app.aes_key')),0,16);
+    }
+
+    public function encrypt($str='')
+    {
+        $data=openssl_encrypt($str,"AES-128-CBC",$this->aes_key,OPENSSL_RAW_DATA,$this->aes_iv);
+        $data=base64_encode($data);
+
+        return $data;
+    }
+
+    public function decrypt($str)
+    {
+        $str=base64_decode($str);
+        $data=openssl_decrypt($str,"AES-128-CBC",$this->aes_key,OPENSSL_RAW_DATA,$this->aes_iv);
+
+        return $data;
+    }
+
+}
---
+++ application/common/lib/Auth.php
@@ -0,0 +1,66 @@
+<?php
+namespace app\common\lib;
+
+use think\Cache;
+
+class Auth
+{
+    public static function setPassword($data)
+    {
+        return md5($data.config('app.password_pre_halt'));
+    }
+
+    /**
+     * 生成sign
+     * @param array $data
+     * @return string
+     */
+    public static function setSign($data=[])
+    {
+        //按字段排序
+        ksort($data);
+        //拼接字符串数据
+        $str=http_build_str($data);
+        //通过aes加密
+        $str=(new Aes())->encrypt($str);
+        //所有字符串转化成大写
+        $str=strtoupper($str);
+
+        return $str;
+    }
+
+    public static function checkSign(array $data)
+    {
+        $str=(new Aes())->decrypt($data['sign']);
+
+        if(empty($str)){
+            return false;
+        }
+
+        parse_str($str,$arr);
+
+        if(!is_array($arr) || empty($arr) || $arr['type'] != $data['type']){
+            return false;
+        }
+
+        if((time()-ceil($arr['time']/1000))>config('app.app_sign_time')){
+            return false;
+        }
+
+        //唯一性判定
+        if(Cache::get($data['sign'])){
+            return false;
+        }
+
+        return true;
+    }
+
+    //生成唯一Token
+    public static function setAppLoginToken($phone='')
+    {
+        $str = md5(uniqid(md5(strtotime(true)),true));
+        $str = sha1($str.$phone);
+
+        return $str;
+    }
+}
