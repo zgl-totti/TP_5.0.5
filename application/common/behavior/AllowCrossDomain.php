e01de777a0da37c4c3f62eea57f079c4796dabfa
zgl <724249769@qq.com>
2020-07-14 23:39:09 +0800

    调整

---
+++ application/common/behavior/AllowCrossDomain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\common\behavior;
+
+use think\Response;
+
+class AllowCrossDomain
+{
+    public function run(&$dispatch)
+    {
+        $host_name = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
+        $headers = [
+            "Access-Control-Allow-Origin" => $host_name,
+            "Access-Control-Allow-Credentials" => 'true',
+            "Access-Control-Allow-Headers" => "x-token,x-uid,x-token-check,x-requested-with,content-type,sign"
+        ];
+
+        if ($dispatch instanceof Response) {
+            $dispatch->header($headers);
+        } else if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            $dispatch['type'] = 'response';
+            $response = new Response('', 200, $headers);
+            $dispatch['response'] = $response;
+        }
+    }
+}
